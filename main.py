496c304bbb0f6f8c9e95ff4bba8011acc3b452cd
felipe_renovato <frenovatoa@outlook.com>
2024-05-22 08:40:42 -0600

    Uso de encabezados.

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import json
 
 if __name__ == '__main__':
     
-    # Con el método post se crea un recurso en el servidor.
-    # En el atributo data se envía la información que se quiere almacenar en el servidor.
     url = 'https://httpbin.org/post'
-    payload = {'nombre': 'felipe', 'curso': 'python', 'nivel': 'intermedio'}
+    payload = { 'nombre': 'felipe', 'curso': 'python', 'nivel': 'intermedio' }
+    # Se pueden enviar n cantidad de encabezados al servidor.
+    headers = { 'Content-Type': 'application/json', 'access-token': '12345' }
     
-    # response = requests.post(url, json=payload)
-    response = requests.post(url, data=json.dumps(payload))
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
     
-    '''
-    - json: POST se encarga de serializar el diccionario en un formato JSON.
-    - data: Nosotros debemos serializar los datos.
-    '''
     print(response.url)
     
     if response.status_code == 200:
-        print(response.content)
+        # print(response.content)
+        # Como cliente, puede ser necesario leer los encabezados que el servidor envía.
+        headers_response = response.headers # Diccionario.
+        print(headers_response)
+        server = headers_response['Server']
+        print(server)
